c925dae45c3c4bcdd974265a991da4c7592f4e96
Jesus Coello <jescoevas@hotmail.com>
2021-05-10 19:32:26 +0200

    Countdown

--- src/app/components/lanzamiento/lanzamiento.component.ts
+++ src/app/components/lanzamiento/lanzamiento.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as countdown from 'countdown';
+
+interface Time{
+    days?:number,
+    hours?:number,
+    minutes?:number,
+    seconds?:number
+}
 
 @Component({
   selector: 'app-lanzamiento',
   templateUrl: './lanzamiento.component.html',
   styleUrls: ['./lanzamiento.component.css']
 })
-export class LanzamientoComponent implements OnInit {
+export class LanzamientoComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  fechaLanzamiento = new Date("2021-06-08")
+  time:Time = {}
+  timerId:number
+
+  constructor() {
+    this.timerId = Number(countdown(this.fechaLanzamiento, (ts) => {
+      this.time = ts
+    }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS))
+   }
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    if(this.timerId){
+      clearInterval(this.timerId)
+    }
   }
 
 }
